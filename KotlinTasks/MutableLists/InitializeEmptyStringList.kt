8c109a690d4c8ff6addb7f310c46399a920e8437
nxteai <NathanielNtow@outlook.com>
2024-09-13 20:43:11 +0100

    Create InitializeEmptyStringList.kt

---
+++ KotlinTasks/MutableLists/InitializeEmptyStringList.kt
@@ -0,0 +1,3 @@
+ val myEmptyList = mutableListOf<String>()
+ //This code initializes an empty mutable list specifically designed to hold strings. 
+// The list is created using the mutableListOf function with the generic type String, allowing for dynamic modifications such as adding or removing elements.
