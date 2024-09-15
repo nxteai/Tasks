a7e89ac0970c12b91ebf384069266081f2d9a4d2
nxteai <NathanielNtow@outlook.com>
2024-09-15 13:38:54 +0100

    Create Divide_Equally.kt

---
+++ KotlinTasks/Integers /Divide_Equally.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val squirrels = readln()!!.toInt()
+    val nuts = readln()!!.toInt()
+
+    println(nuts / squirrels)
+}
+
+//N squirrels found K nuts and decided to split them equally. Calculated how many nuts go for each squirrel.
