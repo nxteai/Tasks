a095ad7c361c553c9ff57fa3e5b94d45b68d6b88
nxteai <NathanielNtow@outlook.com>
2024-09-15 14:18:39 +0100

    Create Square_root.kt

---
+++ KotlinTasks/Integers /Square_root.kt
@@ -0,0 +1,9 @@
+import kotlin.math.sqrt
+
+fun main() {
+    val number = readln().toDouble()
+    println(sqrt(number))
+}
+/* This Kotlin code reads a number from the user, converts it to a double, 
+and then prints the square root of the number using the sqrt() function from the kotlin.math library. 
+It simplifies taking user input and immediately calculating and displaying the square root of the inputted value. *\
